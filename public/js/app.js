e8b01f03de5e9365e327b6a50cc2eeaf78f3145f
Leonel Garza Loperena <leonel.garza@prolecge.com>
2022-02-23 11:35:53 -0600

    Cambios para poder correr en Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e)=> {
     msg1.textContent = 'Cargando...'
     msg2.textContent = ''
 
-    fetch('http://localhost:4000/weather?address=' + location).then((response) =>{
+    fetch('/weather?address=' + location).then((response) =>{
         response.json().then((data)=> {
             if (data.error){
                 msg1.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 4000
 
 //define paths for Expressconfig
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -102,6 +103,6 @@ app.get('*', (req, res)=>{
     })
 })
 
-app.listen(4000, () => {
-    console.log('Server is up on port 4000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port + '.')
 })
